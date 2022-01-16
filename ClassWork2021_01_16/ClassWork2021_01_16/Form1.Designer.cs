0ee323f274b59f313429fd6cff559e1116c3e6ca
KirillDonets <97831177+KirillDonets@users.noreply.github.com>
2022-01-16 14:12:54 +0300

    Add files via upload

---
+++ ClassWork2021_01_16/ClassWork2021_01_16/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace ClassWork2021_01_16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.acceptBox = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.onRadio = new System.Windows.Forms.RadioButton();
+            this.offRadio = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.counterBox1 = new System.Windows.Forms.TextBox();
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.label1.Location = new System.Drawing.Point(502, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Моя метка";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(512, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // acceptBox
+            // 
+            this.acceptBox.AutoSize = true;
+            this.acceptBox.Location = new System.Drawing.Point(500, 86);
+            this.acceptBox.Name = "acceptBox";
+            this.acceptBox.Size = new System.Drawing.Size(69, 17);
+            this.acceptBox.TabIndex = 2;
+            this.acceptBox.Text = "Принять";
+            this.acceptBox.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(500, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // onRadio
+            // 
+            this.onRadio.AutoSize = true;
+            this.onRadio.Checked = true;
+            this.onRadio.Location = new System.Drawing.Point(467, 200);
+            this.onRadio.Name = "onRadio";
+            this.onRadio.Size = new System.Drawing.Size(75, 17);
+            this.onRadio.TabIndex = 4;
+            this.onRadio.TabStop = true;
+            this.onRadio.Text = "Включено";
+            this.onRadio.UseVisualStyleBackColor = true;
+            this.onRadio.CheckedChanged += new System.EventHandler(this.onRadio_CheckedChanged);
+            // 
+            // offRadio
+            // 
+            this.offRadio.AutoSize = true;
+            this.offRadio.Location = new System.Drawing.Point(484, 267);
+            this.offRadio.Name = "offRadio";
+            this.offRadio.Size = new System.Drawing.Size(83, 17);
+            this.offRadio.TabIndex = 5;
+            this.offRadio.Text = "Выключено";
+            this.offRadio.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(364, 402);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(267, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(142, 142);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 7;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(114, 221);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 8;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // counterBox1
+            // 
+            this.counterBox1.Location = new System.Drawing.Point(364, 362);
+            this.counterBox1.Name = "counterBox1";
+            this.counterBox1.Size = new System.Drawing.Size(267, 20);
+            this.counterBox1.TabIndex = 9;
+            // 
+            // Timer1
+            // 
+            this.Timer1.Enabled = true;
+            this.Timer1.Interval = 1000;
+            this.Timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(800, 447);
+            this.Controls.Add(this.counterBox1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.offRadio);
+            this.Controls.Add(this.onRadio);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.acceptBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Name = "Form1";
+            this.Text = "Форма";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox acceptBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton onRadio;
+        private System.Windows.Forms.RadioButton offRadio;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.TextBox counterBox1;
+        private System.Windows.Forms.Timer Timer1;
+    }
+}
+
---
+++ ClassWork2021_01_16/ClassWork2021_01_16/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassWork2021_01_16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Label labelx = (Label)sender;
+            labelx.BackColor = Color.Red;
+            MessageBox.Show("Нажата метка " + label2.Text, "Сообщение");
+            labelx.Text = labelx.Text + '+';
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (acceptBox.CheckState == CheckState.Checked)
+            {
+                MessageBox.Show("Принято", "Состояние чекбокса", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+                
+            }
+            if (acceptBox.CheckState == CheckState.Unchecked)
+            {
+                MessageBox.Show("Не нринято", "Состояние чекбокса", MessageBoxButtons.OKCancel, MessageBoxIcon.Stop);
+
+            }
+            if (acceptBox.CheckState == CheckState.Indeterminate)
+            {
+                MessageBox.Show("Не выяснено", "Состояние чекбокса", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+            }
+            monthCalendar1.SelectionStart = new DateTime(2022, 01, 2);
+            monthCalendar1.SelectionEnd = new DateTime(2022, 1, 28);
+        }
+
+        private void onRadio_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radio = sender as RadioButton;
+            if(radio.Checked)
+            {
+                acceptBox.CheckState = CheckState.Unchecked;
+            }
+            else
+            {
+                acceptBox.CheckState = CheckState.Indeterminate;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Text = textBox1.Text;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            DateTimePicker tim = sender as DateTimePicker;
+            Text = tim.Value.ToLongDateString() + ' ' + tim.Value.ToShortTimeString();
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            MonthCalendar tim = sender as MonthCalendar;
+            Text = tim.SelectionStart.ToLongDateString() + " до " + tim.SelectionEnd.ToLongDateString();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(sender != this)
+            {
+                Control control = sender as Control;
+                Point xy = control.Location;
+                Text = $"[{e.X+xy.X}:{e.Y+xy.Y}] {e.Button}";
+
+            }
+            else
+            {
+                Text = $"[{e.X}:{e.Y}] {e.Button}";
+
+            }
+        }
+        int tiks = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counterBox1.Text = tiks++.ToString();
+            if (tiks > 10)
+                Timer1.Stop();
+        }
+    }
+}
